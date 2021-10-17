db4e8f9ac83c488a1098ef0577af65ea4a0fd7b7
Kaigarodov Andrey <kaigarodov2012@yandex.ru>
2021-10-17 21:38:40 +0500

    fix: Code refactoring (SUN, Sep 17, 2021 19:38:24 PM MSK)

--- cssMemSlider/script.js
+++ cssMemSlider/script.js
@@ -2,10 +2,9 @@ let sliders = document.querySelectorAll(".sliders-track__img");
 let slidersContainer = document.querySelector(".slides-track");
 let sliderWrappers = document.querySelectorAll(".slide-wrapper");
 
+let discriptionTrack = document.querySelector(".discriptions-track");
 let discriptions = document.querySelectorAll(".sliders-track__discript");
 let bullets = document.querySelectorAll(".bullets__item");
-
-let discriptionTrack = document.querySelector(".discriptions-track");
 
 let currActiveIndex = 0;
 
@@ -13,29 +12,16 @@ bullets.forEach(
     (el,idx)=> el.addEventListener('click',()=>{
         activeSlide(idx);
     }))
-/*
-sliders.forEach(
-    (el)=> el.addEventListener('transitionend',(el)=>{
-        if(el.target.classList.contains("img_prev")){
-            el.target.classList.remove("img_prev");
-            sliders[currActiveIndex].classList.add("img_open");
-        }
-    }))
 
-
-*/
+    //init
 activeSlide(currActiveIndex);
 
 function activeSlide(index){
     sliders[currActiveIndex].classList.remove("img_active");
-    //sliders[currActiveIndex].classList.remove("img_open");
-    
     discriptions[currActiveIndex].classList.remove("discript_active");
     bullets[currActiveIndex].classList.remove("bullet_active");
 
-
     sliders[index].classList.add("img_active");
-
     discriptions[index].classList.add("discript_active");
     bullets[index].classList.add("bullet_active");
 
@@ -43,6 +29,4 @@ function activeSlide(index){
     discriptionTrack.style.transform = `translateX(${-index*discriptions[index].clientWidth}px)`
 
     currActiveIndex = index;
-
-
 }
